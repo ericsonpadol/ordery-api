3ae8596390918648995c653b8eab39c9410ad5cd
ericson padol <ericson.padol@asurion.com>
2019-07-01 10:32:14 +0800

    initial draft

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/app.info', function() {
+    echo '<pre>' ;
+    echo '<h1>' . config('app.name') . ': Health-Check</h1>';
+    foreach ($_SERVER as $key => $value) {
+        echo '<strong>' . $key . ':</strong> ' . $value .'<br/>';
+    }
+    echo '</pre>';
+});
