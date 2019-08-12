f06410194469f309d0cb63f061f371d362a29f71
Ericson Padol <ericson.padol@gmail.com>
2019-08-12 14:38:48 +0800

    fix on food-category

--- app/Foodcategory.php
+++ app/Foodcategory.php
@@ -6,17 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 use App\Foodmenu;
 use DB;
 use Log;
+use App\Store;
+use App\Traits\StatusHttp;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Foodcategory extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes,
+        StatusHttp;
 
     protected $key = 'id';
     protected $table = 'foodcategories';
     protected $fillable = [
         'food_category_id',
         'food_category_name',
+        'store_id',
     ];
 
     protected $hidden = [
@@ -45,4 +49,51 @@ class Foodcategory extends Model
         return $this->hasMany('App\Foodmenu');
     }
 
+    public function getAllFoodCategoryWithStoreInfo()
+    {
+        //return all food category with store information
+        $Store = new Store();
+
+        $result = DB::table($this->table)
+            ->join($Store->getStoreTable(), $this->table . '.store_id', '=', $Store->getStoreTable() . '.store_id')
+            ->select(
+                $this->table . '.store_id',
+                $this->table . '.food_category_id',
+                $this->table . '.food_category_name',
+                $this->table . '.updated_at',
+                $Store->getStoreTable() . '.store_name'
+            )
+            ->get();
+
+        return [
+            'data' => $result ? $result : [],
+            'http_code' => $this->getStatusCode200(),
+            'status' => __('messages.status_success')
+        ];
+
+    }
+
+    public function getSpecificStoreFoodCategoryWithStoreInfo($id)
+    {
+        //return all food category with store information
+        $Store = new Store();
+
+        $result = DB::table($this->table)
+            ->join($Store->getStoreTable(), $this->table . '.store_id', '=', $Store->getStoreTable() . '.store_id')
+            ->select(
+                $this->table . '.store_id',
+                $this->table . '.food_category_id',
+                $this->table . '.food_category_name',
+                $this->table . '.updated_at',
+                $Store->getStoreTable() . '.store_name'
+            )
+            ->where($this->table . '.store_id', '=', $id)
+            ->get();
+
+        return [
+            'data' => $result ? $result : [],
+            'http_code' => $this->getStatusCode200(),
+            'status' => __('messages.status_success')
+        ];
+    }
 }
